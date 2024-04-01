00184b6673205089ca67e6aa648cf6d1901cc7a0
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-04-01 23:17:54 +0000

    to_df to to_pandas

--- src/ai/r-engine/main.py
+++ src/ai/r-engine/main.py
@@ -8,18 +8,22 @@ data = []
 global table
 table = None
 
+
 def get_recommendations(title):
     pd_data = pd.DataFrame(data)
     # Table Search
-    result = table.search(pd_data[pd_data['title'] == title]["vector"].values[0]).limit(5).to_df()
+    result = table.search(
+        pd_data[pd_data['title'] == title]["vector"].values[0]).limit(5).to_pandas()
 
     # Get IMDB links
-    links = pd.read_csv('./movie-data/links.csv', header=0, names=["movie id", "imdb id", "tmdb id"], converters={'imdb id': str})
+    links = pd.read_csv('./movie-data/links.csv', header=0,
+                        names=["movie id", "imdb id", "tmdb id"], converters={'imdb id': str})
 
     ret = result['title'].values.tolist()
     # Loop to add links
     for i in range(len(ret)):
-        link = links[links['movie id'] == result['id'].values[i]]["imdb id"].values[0]
+        link = links[links['movie id'] ==
+                     result['id'].values[i]]["imdb id"].values[0]
         link = "https://www.imdb.com/title/tt" + link
         ret[i] = [ret[i], link]
     return ret
@@ -28,14 +32,16 @@ def get_recommendations(title):
 if __name__ == "__main__":
 
     # Load and prepare data
-    ratings = pd.read_csv('./movie-data/ratings.csv', header=None, names=["user id", "movie id", "rating", "timestamp"])
+    ratings = pd.read_csv('./movie-data/ratings.csv', header=None,
+                          names=["user id", "movie id", "rating", "timestamp"])
     ratings = ratings.drop(columns=['timestamp'])
     ratings = ratings.drop(0)
     ratings["rating"] = ratings["rating"].values.astype(np.float32)
     ratings["user id"] = ratings["user id"].values.astype(np.int32)
     ratings["movie id"] = ratings["movie id"].values.astype(np.int32)
 
-    reviewmatrix = ratings.pivot(index="user id", columns="movie id", values="rating").fillna(0)
+    reviewmatrix = ratings.pivot(
+        index="user id", columns="movie id", values="rating").fillna(0)
 
     # SVD
     matrix = reviewmatrix.values
@@ -44,16 +50,16 @@ if __name__ == "__main__":
     vectors = np.rot90(np.fliplr(vh))
     print(vectors.shape)
 
-
     # Metadata
-    movies = pd.read_csv('./movie-data/movies.csv', header=0, names=["movie id", "title", "genres"])
+    movies = pd.read_csv('./movie-data/movies.csv', header=0,
+                         names=["movie id", "title", "genres"])
     movies = movies[movies['movie id'].isin(reviewmatrix.columns)]
 
     data = []
     for i in range(len(movies)):
-        data.append({"id": movies.iloc[i]["movie id"], "title": movies.iloc[i]['title'], "vector": vectors[i], "genre": movies.iloc[i]['genres']})
+        data.append({"id": movies.iloc[i]["movie id"], "title": movies.iloc[i]
+                    ['title'], "vector": vectors[i], "genre": movies.iloc[i]['genres']})
     print(pd.DataFrame(data))
-
 
     # Connect to LanceDB
 
@@ -62,7 +68,6 @@ if __name__ == "__main__":
         table = db.create_table("movie_set", data=data)
     except:
         table = db.open_table("movie_set")
-
 
     print(get_recommendations("Moana (2016)"))
     print(get_recommendations("Rogue One: A Star Wars Story (2016)"))
