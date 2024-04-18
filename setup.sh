154b2aec34b3483553241ce6918cb03fb8d45232
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2024-04-19 01:00:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 docker build -t svelte-manga-api-image .
 docker run --name svelte-manga-api-container -d -p 8000:8000 -v $(pwd):/code svelte-manga-api-image
 
-#connect to turborepo
+#connect to turborepo/monorepo
 git subtree add --prefix=apps/svelte-manga-api https://github.com/valiantlynx/svelte-manga-api.git main --squash
 git subtree pull --prefix=apps/svelte-manga-api https://github.com/valiantlynx/svelte-manga-api.git main --squash
 git subtree push --prefix=apps/svelte-manga-api https://github.com/valiantlynx/svelte-manga-api.git main
